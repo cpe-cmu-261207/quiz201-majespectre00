ed2c02fdd131f848c8cb7d677ac999f117ff244a
Sa <oat601@gmail.com>
2021-07-29 14:19:48 +0700

    res + toggle

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
-
-// define more constants and variables here
+const res = document.getElementById('reset')
+const serch = document.getElementById('search')
+let authornum = 640631102+ parseInt(length.value)
+let state = 0
 
 btn_toggle.onclick = () => {
-  // your code here
+  if(state == 0){
+  author.innerHTML = authornum
+  state ++
+
+}else{
+  author.innerHTML = "640631102 PANNAWIT PANWONG"
+  state -- 
+}
 }
 
+res.onclick = () =>{
+  length.value = 5
+  color.value = "#FF0000"
+}
+serch.onclick = () =>{
+  
+}
 // more codes for Search and Reset buttons here
